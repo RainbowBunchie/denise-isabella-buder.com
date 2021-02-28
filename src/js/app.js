a21c4ad8c428884621d59886ecc84cdbfe032649
Denise Buder <denisebuder@Denises-MacBook-Pro-2.local>
2021-02-28 18:29:15 +0100

    feat: ⭐️ hide desktop navigation on mobile

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,8 @@ import '../scss/app.scss';
 
 import './SmoothScroll.js';
 
-Chocolat(document.querySelectorAll('.chocolat'), {});
+Chocolat(document.querySelectorAll('.chocolat'), {
+    loop: true,
+});
 
 Chocolat(document.querySelectorAll('.chocolat2'), {});
