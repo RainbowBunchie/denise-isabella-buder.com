8b7970065f9663173e274b96682e2e7d2ef4d097
Denise Buder <denisebuder@Denises-MacBook-Pro-2.local>
2021-03-05 21:52:08 +0100

    fix: 🐛 close mobile nav on click on item

--- src/js/MobileNav.js
+++ src/js/MobileNav.js
@@ -2,8 +2,22 @@ const burger = document.getElementsByClassName('navigation--burger')[0];
 const nav = document.getElementsByClassName('navigation--mobile')[0];
 const body = document.getElementsByTagName('body')[0];
 
+const liElements = nav.getElementsByClassName('navigation__item');
+
+
 burger.addEventListener('click', () => {
     burger.classList.toggle('open');
     nav.classList.toggle('navigation--open');
     body.classList.toggle('body--no-scroll');
 });
+
+for (const li of liElements) {
+    console.log(li);
+
+    li.addEventListener('click', () => {
+        burger.classList.toggle('open');
+        nav.classList.toggle('navigation--open');
+        body.classList.toggle('body--no-scroll');
+    });
+}
+
