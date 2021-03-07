2f259430c6ffcdf0fe7092e47a11551262889bc5
Denise Buder <denisebuder@Denises-MacBook-Pro-2.local>
2021-03-07 20:13:53 +0100

    fix: 🐛 add animation preload class to body

--- src/js/app.js
+++ src/js/app.js
@@ -7,3 +7,8 @@ import './Lightbox.js';
 import './MobileNav.js';
 
 import './Parallax.js';
+
+window.addEventListener('load', () => {
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('preload');
+});
