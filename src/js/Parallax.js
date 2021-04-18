fdd8b663a426d29ebe363f5d40f29ece49890b3a
Denise Buder <denisebuder@Denises-MacBook-Pro-2.local>
2021-04-18 20:51:36 +0200

    feat: ⭐️ change images and links

--- src/js/Parallax.js
+++ src/js/Parallax.js
@@ -12,3 +12,19 @@ const bottomNav = document.getElementsByClassName('navigation--bottom')[0];
 new ScrollMagic.Scene({ triggerElement: revealElements })
     .setClassToggle(bottomNav, 'navigation--visible') // add class toggle
     .addTo(controller);
+
+// const aboutMe = document.getElementsByClassName('about-me')[0];
+// const aboutMeHeadline = document.getElementsByClassName('about-me__headline')[0];
+
+// new ScrollMagic.Scene({ triggerElement: aboutMeHeadline })
+//     .setClassToggle(aboutMe, 'section--animate') // add class toggle
+//     .addTo(controller);
+
+
+
+// const aboutMe = document.getElementsByClassName('about-me')[0];
+// const aboutMeHeadline = document.getElementsByClassName('about-me__headline')[0];
+
+// new ScrollMagic.Scene({ triggerElement: aboutMeHeadline })
+//     .setClassToggle(aboutMe, 'section--animate') // add class toggle
+//     .addTo(controller);
