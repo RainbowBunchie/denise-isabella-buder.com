3ca28f8e08858dcf882c00c320d4547f741fa6b1
RainbowBunchie <d.buder@sbg.at>
2022-08-03 21:52:53 +0200

    feat: ⭐️ add first animation attempt to character

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+import TheCharacter from '@/components/TheCharacter.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <TheCharacter />
   </main>
 </template>
