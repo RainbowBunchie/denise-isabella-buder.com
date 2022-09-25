3cdcdb35bccf8fe8b30c176e56d45c5ac1400220
RainbowBunchie <d.buder@sbg.at>
2022-09-25 18:13:23 +0200

    feat: ⭐️ add footer stylings

--- src/layout/TheFooter.vue
+++ src/layout/TheFooter.vue
@@ -4,11 +4,26 @@ import { RouterLink } from 'vue-router';
 <template>
   <nav class="nav nav--footer footer">
     <div class="footer__links">
-      <RouterLink to="/data-policy">Data Policy</RouterLink>
-      <RouterLink to="/imprint">Imprint</RouterLink>
-
-      <font-awesome-icon :icon="['fab', 'github']" class="icon" />
-      <font-awesome-icon :icon="['fab', 'linkedin']" class="icon" />
+      <div class="links__socials socials">
+        <a
+          class="socials__link"
+          href="https://github.com/RainbowBunchie"
+          target="_blank"
+        >
+          <font-awesome-icon :icon="['fab', 'github']" class="icon" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/denise-buder-5ab783122/"
+          target="_blank"
+          class="socials__link"
+        >
+          <font-awesome-icon :icon="['fab', 'linkedin']" class="icon" />
+        </a>
+      </div>
+      <RouterLink class="links__link link" to="/data-policy"
+        >Data Policy</RouterLink
+      >
+      <RouterLink class="links__link link" to="/imprint">Imprint</RouterLink>
     </div>
     <div class="footer__copyright">© 2022 Denise Buder</div>
   </nav>
@@ -20,7 +35,46 @@ import { RouterLink } from 'vue-router';
   width: 100vw;
   &__links {
     background-color: $color--primary;
-    height: 400px;
+    padding: 10rem 0;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+
+    .link {
+      font-family: $font--heading;
+      font-size: 1.5rem;
+      font-weight: 900;
+      text-decoration: none;
+      text-transform: uppercase;
+      color: $color--secondary;
+      transition: all 0.25s ease;
+      &:hover {
+        color: $color--accent;
+      }
+    }
+
+    .socials {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      gap: 1rem;
+      &__link {
+        transition: all 0.35s ease;
+
+        color: $color--secondary;
+        height: 40px;
+        &:hover {
+          transform: rotate(-45deg);
+          color: $color--accent;
+        }
+        .icon {
+          height: 40px;
+          width: 40px;
+        }
+      }
+    }
   }
   &__copyright {
     padding: 2rem;
