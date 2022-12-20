966d302991e41d198832d6e10be6497fa72ff302
RainbowBunchie <d.buder@sbg.at>
2022-12-20 22:20:58 +0100

    style: 💄 fix import of fontawesome

--- src/utilities/fontawesome.ts
+++ src/utilities/fontawesome.ts
@@ -3,6 +3,6 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faGithub, faLinkedin, faEnvelope);
+library.add(faGithub, faEnvelope, faLinkedin);
 
 export default FontAwesomeIcon;
